dc1ada5ddf05aa5c6e50a09f4b837db522ba9fad
Josh Kocur (jak472) <jak472@mail.usask.ca>
2017-10-10 21:24:36 -0600

    I added a simple credit card function, it can check to see if there is a valid entry for the card's expiration date.

---
+++ 370Project-v2.0/src/Payment/credit.java
@@ -0,0 +1,59 @@
+package Payment;
+import java.util.Date;
+
+/**
+ * Created by Kocur on 2017-10-10.
+ */
+public class credit {
+
+    private int number;
+    private int cvs;
+    private String cardType;
+    private Date goodThru;
+    private Date validFrom;
+
+    /*
+        Creator Method. In order pass card num, cvs, card type, expiration date and starting date
+     */
+    credit(int num, int code, String type, Date expiry, Date startDate)
+    {
+        this.number = num;
+        this.cvs = code;
+        this.cardType = type;
+        this.goodThru = expiry;
+        this.validFrom = startDate;
+    }
+
+    /**
+        IN: Credit Card
+        OUT: Returns true if the expiration date is after the "validFrom" date
+     */
+    public boolean isDateValid(credit c)
+    {
+        if(c.validFrom.compareTo(c.goodThru) <= 0)
+        {
+            throw new  RuntimeException("Error:  Credit Card expiration date is invalid");
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    /**
+     * Testing date functions
+     */
+    public static void main(String[] args)
+    {
+
+     Date testDate1 = new Date (1994, 3, 8 );
+     Date testDate2 = new Date (1994, 3, 9);
+
+     credit testCard = new credit(123, 123,"Visa", testDate1, testDate2);
+
+     testCard.isDateValid(testCard);
+    }
+
+
+
+}
