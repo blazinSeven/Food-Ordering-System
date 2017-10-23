a5739f05a602705e66cbacbd7d0c14f9acc7e298
Josh Kocur (jak472) <jak472@mail.usask.ca>
2017-10-22 18:36:24 -0600

    Josh:  Added a food class, added the beginning of the "addDishes" class.

--- 370Project/src/database/AddDishes.java
+++ 370Project/src/database/AddDishes.java
@@ -1,4 +1,25 @@
+/**
+ * Created by Josh on October 22nd
+ */
+
 package database;
+import Entities.Food;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class AddDishes {
+    GoConnection connection = new GoConnection();
+    Food toAdd;
+
+    public AddDishes(Food newItem)
+    {
+        this.toAdd = newItem;
+    }
+
+
+    public void setDishInfo()
+
+
+
 }
